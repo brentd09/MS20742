896b31372011d770391fde8bfc4d545d8b71fbab
Administrator@Adatum.com <Administrator@Adatum.com>
2024-06-17 20:42:16 -0700

    Added a script to compare replicated data

---
+++ Compare-ADReplicatedData.ps1
@@ -0,0 +1,26 @@
+﻿[CmdletBinding()]
+Param (
+  [string]$Identity = 'Abbi'
+)
+
+Get-ADReplicationAttributeMetadata -Object (Get-ADUser -Identity $Identity) -Server tor-dc1 |
+  Select-Object -Property @{n='Identity';e={$Identity}},
+                          @{n='Name';e={$_.AttributeName}}, 
+                          @{n='OrigDC';e={$_.LastOriginatingChangeDirectoryServerInvocationId }},
+                          @{n='OrigTime';e={$_.LastOriginatingChangeTime }},
+                          @{n='OrigUSN';e={$_.LastOriginatingChangeUSN }},
+                          @{n='LocalServer';e={$_.Server }},
+                          @{n='LocalUSN';e={$_.LocalChangeUsn}},
+                          Version |
+Format-Table
+
+Get-ADReplicationAttributeMetadata -Object (Get-ADUser -Identity $Identity) -Server lon-dc1 |
+  Select-Object -Property @{n='Identity';e={$Identity}},
+                          @{n='Name';e={$_.AttributeName}}, 
+                          @{n='OrigDC';e={$_.LastOriginatingChangeDirectoryServerInvocationId }},
+                          @{n='OrigTime';e={$_.LastOriginatingChangeTime }},
+                          @{n='OrigUSN';e={$_.LastOriginatingChangeUSN }},
+                          @{n='LocalServer';e={$_.Server }},
+                          @{n='LocalUSN';e={$_.LocalChangeUsn}},
+                          Version |
+Format-Table
