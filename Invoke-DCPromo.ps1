32c74be888a7231f74dc76c2366eacb47b65b223
Administrator@ADATUM.COM <Administrator@ADATUM.COM>
2024-06-17 17:25:13 -0700

    added dcpromo script

---
+++ Invoke-DCPromo.ps1
@@ -0,0 +1,11 @@
+﻿[cmdletbinding()]
+Param (
+  $AdminCreds = (Get-Credential -UserName 'Adatum\Administrator' -Message 'Enter the Domain Credential'),
+  $Domain = 'adatum.com'
+)
+# Installing the ADDS Roles and Features
+Add-WindowsFeature AD-Domain-Services, RSAT-AD-AdminCenter, RSAT-ADDS-Tools 
+
+# Promoting the Server to become a DC
+Install-ADDSDomainController -DomainName $Domain -InstallDNS:$True -Credential $AdminCreds -SafeModeAdministratorPassword $AdminCreds.Password -NoGlobalCatalog -Force 
+ 
